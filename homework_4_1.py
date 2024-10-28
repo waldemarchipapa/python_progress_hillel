73e9bf7c2ced816cc5650b0eabd15fe30cb10061
waldemar <valodya.top1@gmail.com>
2024-10-28 23:32:36 +0200

    Revert "changes"
    This reverts commit fb0454e7efe38b3e1e2fef555cadc696de457e73.

--- homework_4_1.py
+++
@@ -1,8 +0,0 @@
-lst = [1, 0, 2, 0, 3, 0, 4, 0, 5]
-i = 0
-while i < len(lst):
-    if lst[i] == 0:
-        del lst[i]
-        lst.append(0)
-    i += 1
-print(lst)
--- homework_4_3.py
+++
@@ -1,4 +0,0 @@
-import random
-my_list = [random.randint(1, 100) for i in range(random.randint(3, 10))]
-print(my_list)
-print([my_list[0], my_list[2], my_list[-2]])
